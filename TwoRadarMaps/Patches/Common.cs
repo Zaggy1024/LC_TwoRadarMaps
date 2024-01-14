afc56fd405ece7e07f40490328e7d4f0ab4b3b1a
Zaggy1024 <Zaggy1024@gmail.com>
2024-01-14 14:27:00 -0600

    Move member information used for injection to a Reflection class

--- TwoRadarMaps/Patches/Common.cs
+++ TwoRadarMaps/Patches/Common.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Reflection.Emit;
-using System.Reflection;
 
 using HarmonyLib;
 
@@ -8,17 +7,14 @@ namespace TwoRadarMaps.Patches
 {
     internal class Common
     {
-        static readonly FieldInfo f_StartOfRound_mapScreen = typeof(StartOfRound).GetField(nameof(StartOfRound.mapScreen));
-        static readonly FieldInfo f_Plugin_terminalMapRenderer = typeof(Plugin).GetField(nameof(Plugin.terminalMapRenderer));
-
         public static IEnumerable<CodeInstruction> TranspileReplaceMainWithTerminalMapRenderer(IEnumerable<CodeInstruction> instructions)
         {
             foreach (var instruction in instructions)
             {
-                if (instruction.LoadsField(f_StartOfRound_mapScreen))
+                if (instruction.LoadsField(Reflection.f_StartOfRound_mapScreen))
                 {
                     yield return new CodeInstruction(OpCodes.Pop);
-                    yield return new CodeInstruction(OpCodes.Ldsfld, f_Plugin_terminalMapRenderer);
+                    yield return new CodeInstruction(OpCodes.Ldsfld, Reflection.f_Plugin_terminalMapRenderer);
                 }
                 else
                 {
---
+++ TwoRadarMaps/Patches/Reflection.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+namespace TwoRadarMaps.Patches
+{
+    public static class Reflection
+    {
+        public static readonly FieldInfo f_StartOfRound_mapScreen = typeof(StartOfRound).GetField(nameof(StartOfRound.mapScreen));
+        public static readonly FieldInfo f_Plugin_terminalMapRenderer = typeof(Plugin).GetField(nameof(Plugin.terminalMapRenderer));
+    }
+}
