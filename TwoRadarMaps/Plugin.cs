4c24589ba47afafccc2d24307690cd33990387a9
Zaggy1024 <Zaggy1024@gmail.com>
2024-02-14 18:57:35 -0600

    Use simplified `new()` to create `Harmony` instance

--- TwoRadarMaps/Plugin.cs
+++ TwoRadarMaps/Plugin.cs
@@ -20,7 +20,7 @@ namespace TwoRadarMaps
         private const string MOD_UNIQUE_NAME = "Zaggy1024." + MOD_NAME;
         private const string MOD_VERSION = "1.1.2";
 
-        private readonly Harmony harmony = new Harmony(MOD_UNIQUE_NAME);
+        private readonly Harmony harmony = new(MOD_UNIQUE_NAME);
 
         public static Plugin Instance { get; private set; }
         public new ManualLogSource Logger => base.Logger;
