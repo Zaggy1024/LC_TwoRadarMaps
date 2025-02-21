572ecc599a06cabd896593fab817f0657a806f88
Zaggy1024 <Zaggy1024@gmail.com>
2025-02-20 23:21:57 -0600

    Use the MOD_NAME constant for the assembly name

--- TwoRadarMaps/Properties/AssemblyInfo.cs
+++ TwoRadarMaps/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TwoRadarMaps")]
+[assembly: AssemblyTitle(Plugin.MOD_NAME)]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TwoRadarMaps")]
+[assembly: AssemblyProduct(Plugin.MOD_NAME)]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
