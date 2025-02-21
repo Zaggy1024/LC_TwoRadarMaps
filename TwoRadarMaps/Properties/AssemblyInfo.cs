7f89da95698b5dca24aef1f3fb6db83f1dc20e64
Zaggy1024 <Zaggy1024@gmail.com>
2025-02-20 23:21:34 -0600

    Bump the assembly copyright year

--- TwoRadarMaps/Properties/AssemblyInfo.cs
+++ TwoRadarMaps/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using OpenBodyCams;
+using OpenBodyCams;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TwoRadarMaps")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
